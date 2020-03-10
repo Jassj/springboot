e8265d01e6d75f7aff330928e5f1df290977287f
yuanjie <172161401@qq.com>
2020-03-10 18:06:52 +0800

    JPA and Schedule

---
+++ src/main/java/com/yuanjie/springboot/bean/User.java
@@ -0,0 +1,40 @@
+package com.yuanjie.springboot.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * description: 用户实体类
+ * @author yuanjie 2020/03/09 15:54t
+ */
+@Entity // User进行持久化
+public class User {
+    @Id // 声明主键
+    private String userId;
+    private String userName;
+    private String password;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/yuanjie/springboot/schedule/SchedulingConfig.java
@@ -0,0 +1,21 @@
+package com.yuanjie.springboot.schedule;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * description:
+ * 定时任务配置类
+ * @author yuanjie 2020/03/09 16:31
+ */
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+
+    @Scheduled(cron = "10 * 1 * * ?") // 定时任务配置表达式
+    public void demoScheduler() {
+        System.out.println(System.currentTimeMillis());
+    }
+
+}
