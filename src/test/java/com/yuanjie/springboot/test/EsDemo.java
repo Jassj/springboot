45cec22b7c3a740cf44e8e277bdad5727886848a
袁杰 <172161401@qq.com>
2020-11-06 22:00:02 +0800

    style: remove undo code

--- src/test/java/com/yuanjie/springboot/test/EsDemo.java
+++ src/test/java/com/yuanjie/springboot/test/EsDemo.java
@@ -1,6 +1,10 @@
 package com.yuanjie.springboot.test;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.yuanjie.springboot.common.ESClient;
+import com.yuanjie.springboot.common.client.FeignClient;
+import com.yuanjie.springboot.common.client.TideEsApi;
 import org.elasticsearch.action.get.GetRequestBuilder;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.client.transport.TransportClient;
@@ -28,6 +32,19 @@ public class EsDemo {
         GetResponse gr = grb.get();
 
         System.out.println(gr.getSourceAsString());
+    }
+
+
+    public static void main(String[] args) {
+//        TideEsApi ES_API = FeignClient.getInstance().getCommonService(TideEsApi.class, FeignClient.ES_QUERY_URL, "3e300222-7dd6-45db-a1d5-6c26ca626591");
+//        JSONObject request = new JSONObject();
+//        request.put("unionid", "ohmdTt-Oll3-mLzSS16eZtGO7_aU");
+//        String FEATURE_OFFLINE_INDEX = "salm-memberflightfeature-offline-*";
+//        String FEATURE_OFFLINE_TEMPLATE = "query_by_unionid";
+//        TideEsApi.QueryResponse response = ES_API.queryEs(request, FEATURE_OFFLINE_INDEX, FEATURE_OFFLINE_TEMPLATE, "ohmdTt-Oll3-mLzSS16eZtGO7_aU");
+//        System.out.println(JSON.toJSONString(response));
+
+
     }
 
 }
